0d411c36ebe844cd67b409ffdedecf0632d229d1
krikkemd <krikkemd@gmail.com>
2021-04-04 18:27:45 +0200

    fixed bug where logged in user gets redirected to /login on refresh

--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -1,4 +1,4 @@
-import { SET_ERRORS, SET_CURRENT_USER, CLEAR_ERRORS } from '../types';
+import { SET_ERRORS, CLEAR_ERRORS, SET_CURRENT_USER } from '../types';
 import axios from '../../config/axios';
 
 const baseUrl = 'http://localhost:1337/api/v1/getCurrentLoggedInUser';
@@ -10,14 +10,15 @@ export const getCurrentLoggedInUser = () => dispatch => {
     .get(`${baseUrl}`)
     .then(res => {
       console.log(res.data);
-      dispatch({
-        type: CLEAR_ERRORS,
-      });
 
       // Dispatch to userReducer
       dispatch({
         type: SET_CURRENT_USER,
         payload: res.data,
+      });
+
+      dispatch({
+        type: CLEAR_ERRORS,
       });
     })
     // no current user
